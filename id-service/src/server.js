1ca0fd4b3673e079d0b171323509c6e3c54024d2
Yoni1999 <jonathan.molina@alu.ucm.cl>
2025-07-28 09:16:56 -0400

    corrección server

--- id-service/src/server.js
+++ id-service/src/server.js
@@ -8,11 +8,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-
 const PORT = process.env.PORT || 5007;
 
-app.listen(PORT, async () => {
-  await connectProducer(); 
+app.listen(PORT, () => {
   console.log(`🚀 ID Service running on port ${PORT}`);
 });
