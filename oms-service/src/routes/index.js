3ade417c12f2078cd4cb83616b17c311113915f6
Marcelo Cancino <marcelo.cancino.2001@gmail.com>
2025-09-01 09:03:08 -0400

    update

--- oms-service/src/routes/index.js
+++ oms-service/src/routes/index.js
@@ -1,14 +1,9 @@
 // routes/index.js
 const express = require('express');
 const router = express.Router();
-const geofencesRoutes = require('./Geofences.Routes');
 
 const OrdersRoutes = require('./Orders.Routes')
 
-
-
 router.use('/orders', OrdersRoutes);
-
-
 
 module.exports = router;
