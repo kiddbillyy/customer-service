c488c4598267496563033ea8991dfb16e8e99c94
Yoni1999 <jonathan.molina@alu.ucm.cl>
2025-09-05 17:48:54 -0400

    nuevo get comerce-service

--- comerce-service/src/routes/Store.Routes.js
+++ comerce-service/src/routes/Store.Routes.js
@@ -1,10 +1,11 @@
 // routes/companyRoutes.js
 const { Router } = require('express');
-const { postStore, getStore, getStores, putStore } = require('../controllers/Store.Controller');
+const { postStore, getStore, getStores,getStoresBasic, putStore } = require('../controllers/Store.Controller');
 
 
 const router = Router();
 
+router.get('/get', getStoresBasic);
 router.get('/', getStores);
 router.post('/Crear', postStore);
 router.get('/:id', getStore);
