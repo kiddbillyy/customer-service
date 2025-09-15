67ad4a7f62b9a85d0aca12b656c98027d2ab2268
Marcelo Cancino <marcelo.cancino.2001@gmail.com>
2025-09-15 17:52:04 -0300

    Fix time for request in oms service and api-gateway

--- api-gateway/src/proxy/routeFactory.js
+++ api-gateway/src/proxy/routeFactory.js
@@ -3,6 +3,16 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 import createBreaker from '../utils/createBreaker.js';
 import auth from '../middlewares/auth.js';
 import rbac from '../middlewares/rbac.js';
+import http from 'node:http';
+
+
+const httpAgent = new http.Agent({
+  keepAlive: true,
+  keepAliveMsecs: 10_000,
+  maxSockets: 1024,
+  maxFreeSockets: 256
+});
+
 
 export function makeRoute({ path, target,requireAuth = false, requireRbac = false, publicPaths = [], prependBasePath = true }) {
   const breaker = createBreaker(target);
@@ -24,13 +34,26 @@ export function makeRoute({ path, target,requireAuth = false, requireRbac = fals
     createProxyMiddleware({
       target,
       changeOrigin: true,
+      agent: httpAgent,
       pathRewrite: prependBasePath
         ? (incomingPath) => `${path}${incomingPath}`
         : undefined,
-      timeout: 15000,
-      proxyTimeout: 15000,
+/*       timeout: 15000,
+      proxyTimeout: 15000, */
+      timeout: 75_000,      // tiempo total para establecer/recibir respuesta
+      proxyTimeout: 70_000, // inactividad del socket con el destino
+      onProxyReq(proxyReq) {
+        // ayuda a proxies intermedios a no cerrar el socket
+        proxyReq.setHeader('Connection', 'keep-alive');
+      },
       onError: (err, _req, res) => {
-        console.error(`[ProxyError] ${target}:`, err.message);
+        /* console.error(`[ProxyError] ${target}:`, err.message); */
+        console.error('[ProxyError]', {
+          target,
+          code: err.code,
+          message: err.message,
+          name: err.name
+        });
         res.status(502).json({ message: 'Microservicio no disponible' });
       }
     })
